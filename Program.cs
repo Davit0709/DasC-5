f5a929d7762945e19eda2c7a652ac71229c5cb1a
Davit <dav0709@icloud.com>
2023-03-10 01:54:37 +0400

    fu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+       
+        int length = (int)Math.Floor(Math.Log10(number)) + 1;
+
+        if (length < 2) {
+            Console.WriteLine("нет");
+        } else {
+            int digit = (int)(number / Math.Pow(10, length - 2)) % 10;
+            Console.WriteLine(digit);
+        }
+    }
+}
